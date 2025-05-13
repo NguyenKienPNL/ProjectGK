f15e897fb6fd3b9384e451b5213aa30f4af43dac
Your Name <your_email@example.com>
2025-05-13 21:29:27 +0700

    Grass.java

--- src/uet/oop/bomberman/entities/Grass.java
+++ src/uet/oop/bomberman/entities/Grass.java
@@ -3,11 +3,22 @@ package uet.oop.bomberman.entities;
 import javafx.scene.image.Image;
 
 public class Grass extends Entity {
+    private boolean canWalkThrough;
+    private boolean canPlaceBomb;
 
     public Grass(int x, int y, Image img) {
         super(x, y, img);
+        this.canPlaceBomb = true;
+        this.canWalkThrough = true;
     }
 
+    public boolean isCanPlaceBomb() {
+        return canPlaceBomb;
+    }
+
+    public boolean isCanWalkThrough() {
+        return canWalkThrough;
+    }
     @Override
     public void update() {
 
